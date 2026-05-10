619287e05bc435d817a09135f8aa8b19743cef3d
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-05-10 10:51:53 +0530

    [Vikash Prajapati]: remove define block causing axios error

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',
-  },
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
