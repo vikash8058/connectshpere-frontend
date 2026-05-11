5d73d790dfac94fd9366e522f3ea210be51f435f
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-05-11 15:40:25 +0530

    [Vikash Prajapati]: add ngrok header to all requests via interceptor

--- src/api/client.js
+++ src/api/client.js
@@ -15,9 +15,12 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
+  
+  // Add ngrok header to ALL requests including file uploads
+  config.headers['ngrok-skip-browser-warning'] = 'true';
+  
   return config;
 });
-
 api.interceptors.response.use(
   (res) => res,
   (err) => {
